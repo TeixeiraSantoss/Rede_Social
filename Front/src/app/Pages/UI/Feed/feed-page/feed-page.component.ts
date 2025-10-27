cc1b0f3f5e00544a67793422ee202432b4cd1f6d
TeixeiraSantoss <matheusteixasantos1@gamil.com>
2025-10-27 11:27:44 -0300

    Front - Barra de Pesquisa implementada e funcional

--- Front/src/app/Pages/UI/Feed/feed-page/feed-page.component.ts
+++ Front/src/app/Pages/UI/Feed/feed-page/feed-page.component.ts
@@ -16,7 +16,57 @@ export class FeedPageComponent {
 
   usuario: UsuarioReadDTO | null = null
 
+  //Requisitos para aplicar a barra de pesquisa
+  //1º- lista de usuarios e uma lista com os usuarios filtrados
+  usuarios: UsuarioReadDTO[] = []
+  usuariosFiltrados: UsuarioReadDTO[] = []
+
+  //2º- Termo de pesquisa
+  private _termoPesquisa: string = ""
+
+  //3º- Getter e Setter para manipular o termo de pesquisa e manipular a lista de usuarios
+  //Getter
+  public get termoPesquisa(): string{
+    return this._termoPesquisa;
+  }
+
+  //Setter
+  public set termoPesquisa(value: string){
+    this._termoPesquisa = value;
+
+    //Caso "termoPesquisa" contenha algum valor a condição é atendida e o filtro é aplicado
+    if(value){
+      this.usuariosFiltrados = this.filtrarUsuario(this.termoPesquisa)
+    }else{
+      this.usuariosFiltrados = []
+    }
+  }
+
+  //4º- metodo que vai aplicar a logica da pesquisa
+  filtrarUsuario(termoPesquisa: string): UsuarioReadDTO[]{
+    //Recebe o termo de pesquisa e trata alguns casos
+    termoPesquisa = termoPesquisa.toLowerCase() //Trasnforma os caracteres em minusculo
+    .normalize("NFD")                           //Separa os acentos dos caracteres
+    .replace(/[\u0300-\u036f]/g, "")            //Retira os acentos dos caracteres
+    .trim()                                     //Remove os espaços vazios
+
+    //Filtro que vai ter a logica da pesquisa
+    //o filtro vai selecionar os usuarios pelo UserName e pelo Nome
+    return this.usuarios.filter(u => u.userName.toLowerCase().includes(termoPesquisa) ||
+      u.nome.toLowerCase().includes(termoPesquisa));
+
+  }
+
   ngOnInit():void{
+    this.client.get<UsuarioReadDTO[]>("https://localhost:7088/api/usuario/listar")
+    .subscribe({
+      next: (listUsuarios) => {
+        this.usuarios = listUsuarios
+      },
+      error: (erro) => {
+        console.log(erro)
+      }
+    })
     this.usuario = this.auth.getUsuario()
     console.log(this.usuario)
   }
@@ -28,5 +78,9 @@ export class FeedPageComponent {
   navegarPerfilPage():void{
     this.router.navigate(["perfil"])
   }
-  
+
+  navegarPerfilUserFiltrado(id: number):void{
+    this.router.navigate([`perfil/${id}`])
+  }
+   
 }
